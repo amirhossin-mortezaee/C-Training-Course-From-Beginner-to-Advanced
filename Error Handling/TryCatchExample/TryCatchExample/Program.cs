bbb0e56b31ad9ffdd3368a9c2c47f658c9135f3e
AmirHossin Mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2025-03-17 12:47:29 +0330

    TryCatchExample

---
+++ Error Handling/TryCatchExample/TryCatchExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TryCatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter your first number : ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter your second number : ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int result = num1 / num2;
+                Console.WriteLine($"Result : {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid integer.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero! Please enter a nonzero number.");
+            }
+            finally
+            {
+                Console.WriteLine("Program execution completed.");
+            }
+        }
+    }
+}
